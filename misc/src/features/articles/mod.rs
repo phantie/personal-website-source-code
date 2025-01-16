ae40911f953b54beeab1b27d179d5aa1b726d190
phantie <funtom1605@gmail.com>
2025-01-16 21:54:06 +0200

    feat(articles): render article

--- misc/src/features/articles/mod.rs
+++ misc/src/features/articles/mod.rs
@@ -3,6 +3,7 @@
 use leptos::html::article;
 use leptos::prelude::*;
 use leptos::Params;
+use leptos_meta::Stylesheet;
 use leptos_router::components::Outlet;
 use leptos_router::hooks::{use_params, use_query};
 use leptos_router::params::Params;
@@ -44,6 +45,19 @@ struct ArticleParams {
     id: Option<ArticleId>,
 }
 
+pub fn parse_md(markdown_input: &str) -> String {
+    use pulldown_cmark::{html, Options, Parser};
+
+    let mut options = Options::empty();
+    options.insert(Options::ENABLE_STRIKETHROUGH);
+    let parser = Parser::new_ext(markdown_input, options);
+
+    let mut html_output = String::new();
+    html::push_html(&mut html_output, parser);
+
+    html_output
+}
+
 #[component]
 fn Article() -> impl IntoView {
     let params = use_params::<ArticleParams>();
@@ -63,18 +77,25 @@ fn Article() -> impl IntoView {
     let get_article_resource = Resource::new(id, |id| async move { get_article(id).await });
 
     view! {
+        // <Stylesheet href="https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/3.0.1/github-markdown.min.css"/>
+        <Stylesheet href="https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/5.8.1/github-markdown.min.css"/>
         <div class="articles-article">
-            <Suspense fallback=move || view! { <p>"Loading..."</p> }>
-                {move || get_article_resource.get().map(|article|
-                    match article {
-                        Ok(article) => view! { <h1> {article.title} </h1> }.into_any(),
-                        Err(_) => view! {}.into_any()
-                    }
-                )}
-            </Suspense>
+            // <Suspense fallback=move || view! { <p>"Loading..."</p> }>
+            //     {move || get_article_resource.get().map(|article|
+            //         match article {
+            //             Ok(article) => view! { <h1> {article.title} </h1> }.into_any(),
+            //             Err(_) => view! {}.into_any()
+            //         }
+            //     )}
+            // </Suspense>
 
             <Suspense fallback=move || view! { <p>"Loading..."</p> }>
-                {move || get_article_content_resource.get().map(|content| view! { <p> {content} </p> })}
+                {move || get_article_content_resource.get().map(|content|
+                    {
+                        let raw_html = parse_md(&content.unwrap()); // TODO handle
+                        view! { <div class="markdown-body" inner_html={raw_html}></div> }
+                    })
+                }
             </Suspense>
         </div>
 
