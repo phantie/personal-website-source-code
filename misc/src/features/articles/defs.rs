d94c4b9043db91dabe1e990dd3a886c292c43332
phantie <funtom1605@gmail.com>
2025-01-16 19:44:13 +0200

    feat(articles): impl Article, Articles

--- misc/src/features/articles/defs.rs
+++ misc/src/features/articles/defs.rs
@@ -1,7 +1,10 @@
+use std::collections::HashMap;
 use std::path::{Path, PathBuf};
 
 pub type ArticleId = String;
 pub type ArticleContent = String;
+pub type ArticleTag = String;
+pub type ArticleTitle = String;
 
 pub trait ArticleSource {
     fn load_article_content(&self) -> ArticleContent;
@@ -51,5 +54,62 @@ impl RelativeLocalArticleSources {
             "first" => &self.first,
             _ => &self.not_found,
         }
+    }
+}
+
+#[derive(Clone)]
+struct Article {
+    relative_source: RelativeLocalArticleSource,
+    id: ArticleId,
+    title: ArticleTitle,
+    tags: Vec<ArticleTag>,
+}
+
+struct Articles {
+    pub inner: Vec<Article>,
+
+    not_found: Article,
+    id_to_article: HashMap<ArticleId, Article>,
+}
+
+impl Default for Articles {
+    fn default() -> Self {
+        let not_found = Article {
+            relative_source: RelativeLocalArticleSource {
+                relative_path: "not_found.md".into(),
+            },
+            id: "not_found".into(),
+            title: "Not found".into(),
+            tags: vec![],
+        };
+
+        let articles = vec![
+            Article {
+                relative_source: RelativeLocalArticleSource {
+                    relative_path: "first.md".into(),
+                },
+                id: "first".into(),
+                title: "First article".into(),
+                tags: vec![],
+            },
+            not_found.clone(),
+        ];
+
+        Self {
+            id_to_article: articles
+                .iter()
+                .map(|article| (article.id.clone(), article.clone()))
+                .collect(),
+            not_found,
+            inner: articles,
+        }
+    }
+}
+
+impl Articles {
+    pub fn get_by_id(&self, article_id: ArticleId) -> &Article {
+        self.id_to_article
+            .get(&article_id)
+            .unwrap_or(&self.not_found)
     }
 }
