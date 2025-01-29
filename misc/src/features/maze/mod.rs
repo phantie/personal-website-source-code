bef49b227168e815927c33d09f65ae86dcd39497
phantie <funtom1605@gmail.com>
2025-01-29 19:46:17 +0200

    feat(maze): initital defs Cell, Row, Matrix

--- misc/src/features/maze/mod.rs
+++ misc/src/features/maze/mod.rs
@@ -1,3 +1,64 @@
-pub fn maze_test() {
-    println!("works!");
+pub enum Direction {
+    Left,
+    Right,
+    Up,
+    Down,
+}
+
+pub struct Cell {
+    pub can_move_to: bool,
+    pub name: String,
+}
+
+pub type Row = Vec<Cell>;
+pub type Matrix = Vec<Row>;
+pub type UnpaddedMatrix = Matrix;
+pub type PaddedMatrix = Matrix;
+
+pub mod test_mazes {
+    #![allow(unused)]
+
+    use super::*;
+
+    pub fn block() -> Cell {
+        Cell {
+            can_move_to: false,
+            name: "block".into(),
+        }
+    }
+
+    pub fn path() -> Cell {
+        Cell {
+            can_move_to: true,
+            name: "path".into(),
+        }
+    }
+
+    pub fn n0() -> UnpaddedMatrix {
+        vec![vec![block(), path(), path()]]
+    }
+
+    pub fn symbolize_cell(value: &Cell) -> &str {
+        match value {
+            Cell {
+                can_move_to: false, ..
+            } => "#",
+            Cell {
+                can_move_to: true, ..
+            } => "+",
+        }
+    }
+
+    pub fn simple_display_unpadded_matrix(value: &UnpaddedMatrix) {
+        let _ = value
+            .into_iter()
+            .inspect(|v| {
+                let _ = v
+                    .into_iter()
+                    .inspect(|v| print!("{}", symbolize_cell(*v)))
+                    .collect::<Vec<_>>();
+                println!();
+            })
+            .collect::<Vec<_>>();
+    }
 }
