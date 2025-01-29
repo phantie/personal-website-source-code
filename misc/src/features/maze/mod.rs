bffcf8fafd92113a45f3a70efaecc0b1927f7692
phantie <funtom1605@gmail.com>
2025-01-29 20:14:14 +0200

    feat(maze): fns on dimensions

--- misc/src/features/maze/mod.rs
+++ misc/src/features/maze/mod.rs
@@ -10,10 +10,41 @@ pub struct Cell {
     pub name: String,
 }
 
+pub type Dimension = usize;
 pub type Row = Vec<Cell>;
+/// Assume matrix is not empty
 pub type Matrix = Vec<Row>;
 pub type UnpaddedMatrix = Matrix;
 pub type PaddedMatrix = Matrix;
+
+pub fn matrix_is_not_empty(value: &Matrix) -> bool {
+    !value.is_empty()
+}
+
+pub fn matrix_row_dimension(value: &Matrix) -> Dimension {
+    value.first().unwrap().len()
+}
+
+pub fn row_dimension(value: &Row) -> Dimension {
+    value.len()
+}
+
+/// returns true if all dimensions are equal
+/// returns false otherwise
+pub fn valid_dimensions(value: &Matrix) -> bool {
+    assert!(matrix_is_not_empty(&value));
+    value
+        .into_iter()
+        .all(|row| row_dimension(row) == matrix_row_dimension(value))
+}
+
+pub fn pad_matrix(value: UnpaddedMatrix) -> PaddedMatrix {
+    assert!(valid_dimensions(&value));
+
+    // let mut result: Matrix = Default::default();
+
+    unimplemented!()
+}
 
 pub mod test_mazes {
     #![allow(unused)]
