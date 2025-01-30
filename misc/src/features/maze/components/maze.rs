d465d669ea25ab9ecdaf8b2a9df4022407d9d7fa
phantie <funtom1605@gmail.com>
2025-01-30 19:08:35 +0200

    feat(maze): display block prototype

--- misc/src/features/maze/components/maze.rs
+++ misc/src/features/maze/components/maze.rs
@@ -2,7 +2,29 @@ use leptos::prelude::*;
 
 #[component]
 pub fn MazeComponent() -> impl IntoView {
+    let _blocks = vec![vec![0, 1, 2], vec![3, 4, 5], vec![6, 7, 8]];
+
     view! {
-        <div>{"MAZE!"}</div>
+        <div class="click-maze">
+
+            <div class="click-maze-row">
+                <div class="click-maze-col">
+                1
+                </div>
+                <div class="click-maze-col">
+                2
+                </div>
+            </div>
+            <div class="click-maze-row">
+                <div class="click-maze-col">
+                3
+                </div>
+                <div class="click-maze-col">
+                4
+                </div>
+            </div>
+
+        </div>
+
     }
 }
