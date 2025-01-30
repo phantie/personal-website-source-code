69743a83a6c0bd10c826d97f17c907b80915bad7
phantie <funtom1605@gmail.com>
2025-01-30 14:21:45 +0200

    feat(maze): add example file

--- misc/src/bin/features/maze/core.rs
+++ misc/src/bin/features/maze/core.rs
@@ -1,5 +1,89 @@
-use misc::features::maze::maze_test;
+#![allow(unused)]
+
+use misc::features::maze::*;
 
 fn main() {
-    maze_test();
+    'a: {
+        break 'a;
+
+        let m = test_mazes::n0();
+        let m = pad_matrix(m);
+        test_mazes::simple_display_matrix(&m);
+    }
+
+    'b: {
+        break 'b;
+
+        for d in vec![
+            Direction::Left,
+            Direction::Right,
+            Direction::Up,
+            Direction::Down,
+        ] {
+            dbg!(d);
+
+            let m = test_mazes::n0();
+            let pos = test_mazes::n0_start();
+            let s = MovementState::new(m.clone(), pos);
+            s.validate_pos();
+
+            let steps_to_direction = s.movement_possibility(d);
+            dbg!(steps_to_direction);
+
+            let m = pad_matrix(m);
+            let m = align_matrix(m, d);
+            test_mazes::simple_display_matrix(&m);
+        }
+    }
+
+    'c: {
+        break 'c;
+
+        let m = test_mazes::n0();
+        let pos = test_mazes::n0_start();
+        let mut s = MovementState::new(m.clone(), pos);
+        s.validate_pos();
+        test_mazes::simple_display_matrix(&s.m);
+        println!();
+        test_mazes::simple_display_discovered_matrix(&s.m, s.pos);
+        println!();
+
+        let mut can_move_to = s.can_move_to_directions();
+        println!("You can go to {can_move_to:?}");
+
+        s.move_to_direction_once(Direction::Right);
+        test_mazes::simple_display_discovered_matrix(&s.m, s.pos);
+        println!();
+        let mut can_move_to = s.can_move_to_directions();
+        println!("You can go to {can_move_to:?}");
+
+        s.move_to_direction_once(Direction::Left);
+        test_mazes::simple_display_discovered_matrix(&s.m, s.pos);
+        println!();
+        let mut can_move_to = s.can_move_to_directions();
+        println!("You can go to {can_move_to:?}");
+
+        cmd::input_direction();
+    }
+
+    'd: {
+        let m = test_mazes::n0();
+        let pos = test_mazes::n0_start();
+        let mut s = MovementState::new(m.clone(), pos);
+
+        test_mazes::simple_display_matrix(&s.m);
+        println!();
+
+        loop {
+            test_mazes::simple_display_discovered_matrix(&s.m, s.pos);
+            println!();
+
+            let mut can_move_to = s.can_move_to_directions();
+            println!("You can go to {can_move_to:?}");
+
+            let d = cmd::input_available_direction(can_move_to);
+
+            s.move_to_direction_once(d);
+        }
+    }
 }
