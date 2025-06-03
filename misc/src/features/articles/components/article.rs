a1b921790bd7e6cc76a79014871ac6badf07d85e
phantie <funtom1605@gmail.com>
2025-06-03 19:12:03 +0300

    feat(articles): support footnotes

--- misc/src/features/articles/components/article.rs
+++ misc/src/features/articles/components/article.rs
@@ -228,6 +228,7 @@ fn parse_md(markdown_input: &str) -> String {
     let mut options = Options::empty();
     options.insert(Options::ENABLE_STRIKETHROUGH);
     options.insert(Options::ENABLE_HEADING_ATTRIBUTES);
+    options.insert(Options::ENABLE_FOOTNOTES);
     let parser = Parser::new_ext(markdown_input, options);
 
     let mut html_output = String::new();
